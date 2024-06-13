2df2948cea68dc804d27e7f05b8eab994eadcf3d
Karla Ruan <kyesenia.18@gmail.com>
2024-06-13 14:25:15 -0600

    Initial commit

---
+++ src/main/java/com/nexu/Brands/BrandsApplication.java
@@ -0,0 +1,14 @@
+package com.nexu.Brands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandsApplication {
+
+	public static void main(String[] args) {
+            System.out.println("Hola");
+		SpringApplication.run(BrandsApplication.class, args);
+	}
+
+}
